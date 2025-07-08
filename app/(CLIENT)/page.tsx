d71dd144f7b1a7e166feae5148ef56e0d3b2beb3
nikodean1 <nicholas850@protonmail.com>
2025-07-07 20:05:30 -0500

    Added the WhatWeDo section

--- app/(CLIENT)/page.tsx
+++ app/(CLIENT)/page.tsx
@@ -3,6 +3,7 @@ import AboutSection from "../../components/sections/AboutSection";
 import ContactSection from "../../components/sections/ContactSection";
 import HeroSection from "../../components/sections/HeroSection";
 import HowWeWorkSection from "../../components/sections/HowWeWorkSection";
+import WhatWeDoSection from "../..//components/sections/WhatWeDoSection";
 import TeamSection from "../../components/sections/TeamSection";
 import TestimonialsSection from "../../components/sections/TestimonialsSection";
 import WhyUsSection from "../../components/sections/WhyUsSection";
@@ -14,6 +15,7 @@ export default function Home() {
       <HeroSection/>
       <AboutSection/>
       <HowWeWorkSection/>
+      <WhatWeDoSection/>
       <WhyUsSection/>
       <TestimonialsSection/>
       <TeamSection/>
